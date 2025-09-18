9ae87d05cbbd74462093cf8faa31658a59cbb9f8
Klimchuk-sergei <klim.work@mail.ru>
2025-09-18 20:54:42 +0300

    создана модель course

--- course/models.py
+++ course/models.py
@@ -1,4 +1,13 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    course_image = models.ImageField(
+        upload_to='courses/images/',
+        blank=True,
+        null=True,
+        verbose_name="Course Image"
+    )
