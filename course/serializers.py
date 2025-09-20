c6d1ad4fb9823646628bc089750f060fdcf7ac30
Klimchuk-sergei <klim.work@mail.ru>
2025-09-20 20:43:22 +0300

    lesson_count

--- course/serializers.py
+++ course/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from .models import Course
+from lesson.models import Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+    lessons = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = "__all__"
+
+    def get_lesson_count(self, course):
+        return course.lessons_set.count()
+
+
