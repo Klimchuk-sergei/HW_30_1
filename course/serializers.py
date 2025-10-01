43d5f3b6def8990daaa8994784c2897494fefe87
Klimchuk-sergei <klim.work@mail.ru>
2025-10-01 21:03:57 +0300

    создана модель подписки и её представление

--- course/serializers.py
+++ course/serializers.py
@@ -1,13 +1,18 @@
 from rest_framework import serializers
-from .models import Course
+from .models import Course, Subscription
 from lesson.serializers import LessonShortSerializer
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = ["id", "user", "course", "subscribe_at"]
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField(read_only=True)
-    lessons = LessonShortSerializer(
-        many=True, read_only=True
-    )  # Убрали source="lessons"
+    lessons = LessonShortSerializer(many=True, read_only=True)
+    is_subscribed = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Course
@@ -16,3 +21,10 @@ class CourseSerializer(serializers.ModelSerializer):
 
     def get_lessons_count(self, obj):
         return obj.lessons.count()
+
+    def get_is_subscribed(self, obj):
+        """Проверка на подписку текчщего пользователя на курс"""
+        request = self.context.get("request")
+        if request and request.user.is_authenticated:
+            return Subscription.objects.filter(user=request.user, course=obj).exists()
+        return False
